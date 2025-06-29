645700e843782e070b536fe5b9535af1b7ee6f17
delipouya <d.pouyabahar@mail.utoronto.ca>
2025-06-29 00:33:55 -0400

    checking implementation

--- SpatialPeeler/case_prediction.py
+++ SpatialPeeler/case_prediction.py
@@ -68,7 +68,69 @@ def standalone_logistic(X, y):
     return predicted_prob, coef, stderr, pvals
 
 
+
 def single_factor_logistic_evaluation(adata, factor_key="X_nmf", max_factors=30):
+    all_results = []
+    X = adata.obsm[factor_key]
+    y = adata.obs["status"].values
+    sample_ids = adata.obs["sample_id"].values
+
+    for i in range(min(max_factors, X.shape[1])):
+        print(f"Evaluating factor {i+1}...")
+        Xi = X[:, i].reshape(-1, 1)  # single factor
+        print("X i: ", Xi)
+        print(Xi.min(), Xi.max())
+
+        # Logistic regression with full inference
+        p_hat, coef, stderr, pvals = standalone_logistic(Xi, y)
+        logit_p_hat = logit(p_hat)
+
+        # Residuals
+        raw_residual = y - p_hat
+        pearson_residual = raw_residual / np.sqrt(p_hat * (1 - p_hat))
+        deviance_residual = np.sign(raw_residual) * np.sqrt(
+            -2 * (y * np.log(p_hat) + (1 - y) * np.log(1 - p_hat))
+        )
+        scaled_p_hat = p_hat/np.sqrt(p_hat * (1 - p_hat))
+        scaled_z = Xi.flatten()/np.sqrt(p_hat * (1 - p_hat))
+        random_construct = Xi.flatten() - p_hat
+        scaled_random_contruct = random_construct/np.sqrt(p_hat * (1 - p_hat))
+
+        print(f"p_hat[:5]: {p_hat[:5]}"
+              f"Raw residuals: {raw_residual[:5]}"
+              f"\nPearson residuals: {pearson_residual[:5]}"
+              f"\nDeviance residuals: {deviance_residual[:5]}"
+              )
+
+        # Save the results
+        result = {
+            "factor_index": i,
+            "coef": float(coef[1]),                    # β (slope)
+            "intercept": float(coef[0]),               # β₀
+            "std_err": float(stderr[1]),               # SE(β)
+            "std_err_intercept": float(stderr[0]),     # SE(β₀)
+            "pval": float(pvals[1]),                   # p(β ≠ 0)
+            "pval_intercept": float(pvals[0]),         # p(β₀ ≠ 0)
+            "p_hat": p_hat,
+            "scaled_p_hat": scaled_p_hat,
+            "logit_p_hat": logit_p_hat,
+            "scaled_z": scaled_z,
+            "random_construct": random_construct,
+            "scaled_random_contruct": scaled_random_contruct,
+            "status": y,
+            "sample_id": sample_ids,
+            "raw_residual": raw_residual,
+            "pearson_residual": pearson_residual,
+            "deviance_residual": deviance_residual
+            }
+        
+        all_results.append(result)
+
+    return all_results
+
+
+
+def single_factor_logistic_evaluation_original(adata, factor_key="X_nmf", max_factors=30):
     all_results = []
     X = adata.obsm[factor_key]
     y = adata.obs["status"].values
