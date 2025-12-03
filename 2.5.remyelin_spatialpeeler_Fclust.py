8c65142bda03a940167627ba9443886f41dd8a04
delipouya <d.pouyabahar@mail.utoronto.ca>
2025-12-03 13:43:34 -0500

    clustering the factor scores before feeding into the classifier

--- 2.5.remyelin_spatialpeeler_Fclust.py
+++ 2.5.remyelin_spatialpeeler_Fclust.py
@@ -73,6 +73,10 @@ metadata_df = metadata_df.reset_index(drop=True)
 duplicate_rows_mask = metadata_df.duplicated()
 metadata_df = metadata_df[~duplicate_rows_mask]
 print(metadata_df)
+saline_samples = metadata_df[metadata_df['Condition'] == 'Saline']['sample_id'].tolist()
+lpc_samples = metadata_df[metadata_df['Condition'] == 'LPC']['sample_id'].tolist()
+print("Saline samples: ", saline_samples)
+print("LPC samples: ", lpc_samples)
 
 # Plot spatial maps for the first 8 samples
 #for i in range(min(8, len(sample_ids))):
@@ -152,12 +156,8 @@ def standalone_logistic(X, y):
 
 
 
-#### uncropped - t3_7 indices
-GOF_index = [21, 1, 9, 22, 24]
-LOF_index = [0, 27, 12, 5, 19]
-
 visualize_each_factor = False
-exception_vis = True
+exception_vis = False
 #def single_factor_logistic_evaluation_Fclust(adata, factor_key="X_nmf", max_factors=30):
 factor_key = "X_nmf"
 max_factors = 30
@@ -165,9 +165,15 @@ all_results = []
 X = adata.obsm[factor_key]
 y = adata.obs["status"].values
 sample_ids = adata.obs["sample_id"].values
-i = 0
+i = 9
 
-for i in range(min(max_factors, X.shape[1])):
+t3_7_gof = [9, 21, 18, 11, 1, 2, 5, 23]
+t18_gof = [9, 2, 12, 18]
+t18_lof = [3, 6, 19]
+t7_gof = [0, 12, 20, 2]
+t7_lof = [10, 18, 7]
+
+for i in t3_7_gof: #range(min(max_factors, X.shape[1])) ,3, 6, 19, 
     print(f"Evaluating factor {i+1}...")
     Xi = X[:, i].reshape(-1, 1)  # single factor
     #print("X i: ", Xi)
@@ -192,10 +198,20 @@ for i in range(min(max_factors, X.shape[1])):
     print(remap)
     labels_remapped = np.vectorize(remap.get)(kmeans.labels_).astype(int)
     
+    
+    min_cluster0 = Xi[labels_remapped == 0].min()
+    max_cluster0 = Xi[labels_remapped == 0].max()
+    min_cluster1 = Xi[labels_remapped == 1].min()
+    max_cluster1 = Xi[labels_remapped == 1].max()
+
     print('original labels: ', kmeans.labels_)
     print("Labels remapped: ", labels_remapped)
-    print('cluster-0 min/max:', Xi[labels_remapped == 0].min(), Xi[labels_remapped == 0].max())
-    print('cluster-1 min/max:', Xi[labels_remapped == 1].min(), Xi[labels_remapped == 1].max())
+    print('cluster-0 min/max:', min_cluster0, max_cluster0)
+    print('cluster-1 min/max:', min_cluster1, max_cluster1)
+
+    if min_cluster0 > min_cluster1 or max_cluster0 > max_cluster1:
+        print("Error in clustering remapping!")
+        break   
 
     # Calculate the threshold as the midpoint between the centroids
     threshold = np.mean(centers)
@@ -263,13 +279,35 @@ for i in range(min(max_factors, X.shape[1])):
             'sample_id': adata.obs['sample_id'][high_cluster_indices],
             'p_hat': p_hat
         })
+
         plt.figure(figsize=(10, 10))
-        sns.violinplot(y="p_hat", x="disease",  
-                    data=df_p_hat, inner="box", palette="Set2")
-        plt.legend(title="Sample ID", bbox_to_anchor=(1.05, 1), loc='upper left')
+        sns.violinplot(
+            y="p_hat",
+            x="disease",
+            data=df_p_hat,
+            inner="box",
+            order=["Saline", "LPC"],                     # ensure left→right order
+            palette={"Saline": "skyblue", "LPC": "salmon"}    # enforce specific colors
+        )
         plt.title(f"Factor {i+1} - p_hat Distribution (High-Exp Cluster)")
         plt.tight_layout()
         plt.show()
+
+        plt.figure(figsize=(15, 10))
+        sns.violinplot(
+            y="p_hat",
+            x="sample_id",
+            data=df_p_hat,
+            inner="box",
+            order=saline_samples + lpc_samples,                     # ensure left→right order
+            # salines as green, lpcs as orange
+            palette={sid: "skyblue" for sid in saline_samples} | {sid: "salmon" for sid in lpc_samples}
+        )
+        plt.title(f"Factor {i+1} - p_hat Distribution (High-Exp Cluster)")
+        plt.tight_layout()
+        plt.show()
+
+
 
     if exception_vis:
         ### visualize p-hat values on spatial maps for each sample, for non-high-expression cluster points p_hat=NA
@@ -288,14 +326,17 @@ for i in range(min(max_factors, X.shape[1])):
         figsize=(43, 20), fontsize=45, dot_size=30) #figsize=(42, 30), fontsize=45
 
     
-
-
-# Run factor-wise HiDDEN-like analysis (logistic regression on single factors)
-#results = cpred.single_factor_logistic_evaluation_Fclust(
-#    adata, factor_key="X_nmf", max_factors=optimal_num_pcs_ks
-#)
-
 results = all_results
+results_filename = 'remyelin_nmf30_hidden_logistic_Fclust_t3_7.pkl'
+
+### save the results using pickle
+with open(results_filename, 'wb') as f:
+    pickle.dump(results, f)
+
+# Load
+with open(results_filename, 'rb') as f:
+    results2 = pickle.load(f)
+
 # Extract full model stats for each factor
 coef_list = [res['coef'] for res in results]
 intercept_list = [res['intercept'] for res in results]
@@ -349,289 +390,76 @@ plt.legend()
 plt.show()
 
 
-################################################
-#### uncropped - t3_7 indices 
-GOF_index = [21, 1, 9, 22, 24]
-LOF_index = [0, 27, 12, 5, 19]
-#### uncropped - t3_7 indices - Fclust analysis
-GOF_index = [9, 21, 14, 18, 11, 6, 2, 5]
-LOF_index = [0] #27, 22, 17, 12  all have higehr than 0.05 p-values
+factor_index = 19  # Change this to the desired factor index (0-based)
+adata_sub = adata[results[factor_index]['high_cluster_indices'], :].copy()
+print(adata_sub.shape)
+adata_sub.obs['p_hat'] = results[factor_index]['p_hat'].astype('float32')
+adata_sub.obs['1_p_hat'] = 1 - adata_sub.obs['p_hat'].astype('float32')
+adata_sub_by_sample = {
+    sid: adata_sub[adata_sub.obs['sample_id'] == sid].copy()
+    for sid in adata_sub.obs['sample_id'].unique()
+}
+sample_ids = list(adata_sub_by_sample.keys())
+PATTERN_COND = 'LOF'#'GOF'
+#0:10 are diseased samples, 11:14 are normal samples 
+sample_id_to_check = 1#1#12#6
+an_adata_sample = adata_sub_by_sample[sample_ids[sample_id_to_check]]
+expr_matrix = an_adata_sample.X.toarray() if issparse(an_adata_sample.X) else an_adata_sample.X  # shape: (n_spots, n_genes)
+p_hat_vector = an_adata_sample.obs['p_hat']  # shape: (n_spots,)
+
+neg_p_hat_vector = an_adata_sample.obs['1_p_hat']  # shape: (n_spots,)
+pattern_vector = p_hat_vector if PATTERN_COND == 'GOF' else neg_p_hat_vector
+
+#### removing genes with zero variance
+print(np.all(np.isfinite(expr_matrix)))
+gene_zero_std_index = np.std(expr_matrix, axis=0) == 0
+print(expr_matrix.shape)
+expr_matrix_sub = expr_matrix[:, ~gene_zero_std_index]  # Exclude genes with zero variance
+print(expr_matrix_sub.shape)
+print(np.var(expr_matrix[:, gene_zero_std_index], axis=0)  )
+print(np.var(expr_matrix_sub, axis=0)  )
+
+gene_names = an_adata_sample.var_names[~gene_zero_std_index]
+
+pearson_corr = gid.pearson_correlation_with_pattern(expr_matrix_sub, pattern_vector, 
+                                                     gene_names=gene_names)
+symbols= pearson_corr['gene'].map(hlps.map_ensembl_to_symbol(pearson_corr['gene'].tolist(), species='mouse'))
+pearson_corr['symbols'] = symbols
+
+### sort the pearson correlation dataframe
+pearson_corr.sort_values("correlation", ascending=False, inplace=True)
+print(pearson_corr.head(30))
 
 
-#### uncropped - t18 indices
-GOF_index = [18, 12, 9, 20, 14]
-LOF_index = [3, 19, 23, 25]
-
-#### uncropped - t18 K10 indices
-GOF_index = [6, 2, 0]
-LOF_index = [3, 5, 7]
-
-#### uncropped - t7 indices
-GOF_index = [0, 12, 16, 20, 15]
-LOF_index = [27, 4, 10]
 
 
-print(GOF_index)
-results_LOF = [results[i] for i in LOF_index]
-results_GOF = [results[i] for i in GOF_index]
+
+NMF_idx_values = an_adata_sample.obsm["X_nmf"][:,factor_index]
 
 
-for result in results:
-    print(f"Factor {result['factor_index'] + 1}:")
-    factor_number = result['factor_index'] + 1
-    print(f"  p_hat mean: {result['p_hat'].mean():.4f}")
-    print(f"  Status distribution: {np.bincount(result['status'])}")
+regression_res = gid.regression_with_pattern(expr_matrix_sub, pattern_vector,
+                                               gene_names=gene_names, 
+                                               scale=True)
+regression_corr = pd.DataFrame({
+        "gene": regression_res["gene"],
+        "correlation": regression_res["slope"]})
 
-### creat a heatmap of p_hat for each factor for correlation (30x30)
-p_hat_matrix = np.array([res['p_hat'] for res in results])
-p_hat_df = pd.DataFrame(p_hat_matrix.T,
-                        columns=[f'Factor_{i+1}' for i in range(len(results))],
-                        index=[f'Sample_{i+1}' for i in range(p_hat_matrix.shape[1])])
+regression_corr.sort_values("correlation", ascending=False, inplace=True)
 
-### creat a heatmap of NMF for each factor for correlation (30x30)
-nmf_matrix = adata.obsm["X_nmf"][:, :optimal_num_pcs_ks]
-nmf_df = pd.DataFrame(nmf_matrix,
-                        columns=[f'NMF{i+1}' for i in range(nmf_matrix.shape[1])],
-                        index=[f'Sample_{i+1}' for i in range(nmf_matrix.shape[0])])
 
-for i in range(optimal_num_pcs_ks):
-    p_hat_i = results[i]['p_hat']
-    NMF_i = adata.obsm["X_nmf"][:, i]
-    cropped_status = ['black' if x else  'yellow' for x in adata.obs['cropped'].values]
-
-    ### plot p_hat vs NMF score for the first factor
-    plt.figure(figsize=(6, 6))
-    plt.scatter(NMF_i, p_hat_i, c=cropped_status, alpha=0.7, s=3)
-    plt.xlabel(f"NMF Factor {i+1} Score")
-    plt.ylabel("HiDDEN p_hat")
-    ### set phat range to 0-1
-    plt.ylim(0, 1)
-    plt.title(f"p_hat vs NMF Factor {i+1} Score")
-    plt.grid()
-    plt.tight_layout()
-    plt.show()
-
-### create a heatmap correlation between p_hat and NMF scores for each factor
-### 30x30 - NMF scores x p_hat heatmap matrix
-n_factors = nmf_matrix.shape[1]
-correlation_matrix = np.corrcoef(nmf_matrix.T, p_hat_matrix.T)[:n_factors, n_factors:]  # shape: (30, 30)
-corr_df = pd.DataFrame(correlation_matrix,
-                       index=[f'p_hat_{i+1}' for i in range(n_factors)],
-                       columns=[f'NMF{i+1}' for i in range(n_factors)])
-
-plt.figure(figsize=(20, 12))
-sns.heatmap(corr_df, annot=True, cmap='coolwarm',
-            annot_kws={"size": 8}, fmt=".2f", linewidths=.5)
-plt.title("Correlation Between NMF Factors (x) and p_hat Factors (y)")
-plt.xlabel("NMF Factors")
-plt.ylabel("p_hat Factors")
-plt.tight_layout()
+### make a histogram of the regression coefficients
+plt.figure(figsize=(10, 6))
+sns.histplot(regression_corr['correlation'], bins=30, 
+             kde=False, color='blue', stat='density')
+plt.title("Distribution of Regression Coefficients")
+plt.xlabel("Regression Coefficient Value")
+plt.ylabel("Density")
+plt.legend()
 plt.show()
 
-for result in results_LOF: #results_LOF
-    print(f"Factor {result['factor_index'] + 1}:")
-    factor_number = result['factor_index'] + 1
-    print(f"  p_hat mean: {result['p_hat'].mean():.4f}")
-    print(f"  Status distribution: {np.bincount(result['status'])}")
-
-    adata.obs['p_hat'] = result['p_hat']
-    adata.obs['p_hat'] = adata.obs['p_hat'].astype('float32')
-
-    # Copy adata per sample for plotting
-    sample_ids = adata.obs['sample_id'].unique().tolist()
-    adata_by_sample = {
-        sample_id: adata[adata.obs['sample_id'] == sample_id].copy()
-        for sample_id in sample_ids
-    }
-    # Plot spatial maps for the first 8 samples
-    #for i in range(min(8, len(sample_ids))):
-    #    plot_spatial_p_hat(adata_by_sample[sample_ids[i]], sample_ids[i])
-    adata_by_sample_cropped = {}
-    ### subset the adata_by sample elements by "cropped" in .obs
-
-    for sample_id, adata_sample in adata_by_sample.items():
-        adata_by_sample_cropped[sample_id] = adata_sample[adata_sample.obs['cropped'] == True]
-
-    # For NMF factor  (from obsm)
-    plot.plot_grid(adata_by_sample, sample_ids, key="X_nmf", 
-    title_prefix=f" Factor {result['factor_index'] + 1}- " + "NMF ", counter=factor_number, from_obsm=True, 
-    factor_idx=factor_number-1, figsize=(43, 20), fontsize=45, dot_size=30) #figsize=(45, 15), fontsize=45
-
-    plot.plot_grid(adata_by_sample_cropped, sample_ids, key="X_nmf", 
-    title_prefix=f" Factor {result['factor_index'] + 1}- " + "NMF ", counter=factor_number, from_obsm=True, 
-    factor_idx=factor_number-1, figsize=(43, 20), fontsize=45, dot_size=100) #figsize=(45, 33), fontsize=45
-    
-    # For p_hat #plot.
-    plot.plot_grid(adata_by_sample, sample_ids, key="p_hat", 
-    title_prefix=f" Factor {result['factor_index'] + 1}- " + "HiDDEN predictions", counter=factor_number, 
-    figsize=(43, 20), fontsize=45, dot_size=30) #figsize=(42, 30), fontsize=45
-
-    plot.plot_grid(adata_by_sample_cropped, sample_ids, key="p_hat", 
-    title_prefix=f" Factor {result['factor_index'] + 1}- " + "HiDDEN predictions", counter=factor_number, 
-    figsize=(43, 20), fontsize=45, dot_size=100) #figsize=(42, 30), fontsize=45
+corr_dict = {
+    "Pearson": pearson_corr,
+    "Regression": regression_corr,
+}
 
 
-    df_violin = adata.obs[["sample_id", "p_hat"]].copy()
-    plt.figure(figsize=(5, 7))
-    sns.violinplot(
-        x="sample_id", y="p_hat", hue="sample_id", data=df_violin,
-        palette="Set2", density_norm="width", inner=None, legend=False
-    )
-    sns.boxplot(
-        x="sample_id", y="p_hat", data=df_violin,
-        color="white", width=0.1, fliersize=0
-    )
-    plt.xticks(rotation=45, ha="right")
-    plt.title(f" Factor {result['factor_index'] + 1}- " + "Distribution of p_hat (uncropped)" , fontsize=13)
-    plt.ylim(0, 1)
-    plt.tight_layout()
-    plt.show()
-
-    df_violin_cropped = df_violin[adata.obs['cropped'] == True].copy()
-    plt.figure(figsize=(5, 7))
-    sns.violinplot(
-        x="sample_id", y="p_hat", hue="sample_id", data=df_violin_cropped,
-        palette="Set2", density_norm="width", inner=None, legend=False
-    )
-    sns.boxplot(
-        x="sample_id", y="p_hat", data=df_violin_cropped,
-        color="white", width=0.1, fliersize=0
-    )
-    plt.xticks(rotation=45, ha="right")
-    plt.title(f" Factor {result['factor_index'] + 1}- " + "Distribution of p_hat (cropped)", fontsize=13)
-    plt.ylim(0, 1)
-    plt.tight_layout()
-    plt.show()
-
-
-######################################################
-###### for all factors, plot p-hat vs nmf score as a scatter plot
-for i in range(optimal_num_pcs_ks):
-    nmf_scores = adata.obsm["X_nmf"][:, i]
-    p_hat = results[i]['p_hat']  # p_hat for the i-th factor
-
-    #nmf_scores = nmf_scores[adata.obs['cropped'] == True]
-    #p_hat = p_hat[adata.obs['cropped'] == True]
-
-    plt.figure(figsize=(8, 6))
-    plt.scatter(nmf_scores, p_hat, alpha=0.5, s=10)
-    plt.xlabel(f"NMF Factor {i+1} Score")
-    plt.ylabel("HiDDEN p_hat")
-    plt.title(f"p_hat vs NMF Factor {i+1} Score")
-    plt.grid()
-    plt.tight_layout()
-    plt.show()
-
-
-for i in range(optimal_num_pcs_ks):
-    ### violin plot for nmf scores per sample
-    nmf_scores = adata.obsm["X_nmf"][:, i]
-    nmf_df = pd.DataFrame(nmf_scores,
-                            columns=[f'NMF{i+1}'])
-    nmf_df['sample_id'] = adata.obs['sample_id'].values
-    nmf_long = nmf_df.melt(id_vars='sample_id',
-                            var_name='Factor',
-                            value_name='Score')
-    plt.figure(figsize=(12, 5))
-    sns.violinplot(x="sample_id", y="Score", hue="sample_id",
-                     data=nmf_long, inner="box", palette="Set2")
-    plt.title(f"Distribution of NMF Factor {i+1} Scores per Sample")
-    plt.xticks(rotation=45, ha="right")
-    plt.tight_layout()
-    plt.show()
-
-
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
-######################################################
-############ Identifying Informative factor ############
-# --- Logistic regression on varimax PCs ---
-X = adata.obsm['X_nmf']
-y = LabelEncoder().fit_transform(adata.obs['Condition'].values)
-model = LogisticRegression(penalty=None, solver='lbfgs', max_iter=1000)
-model.fit(X, y)
-coefs = model.coef_[0]
-nmf_labels = [f'NMF{i+1}' for i in range(X.shape[1])]
-importance_df = pd.DataFrame({
-    'Factor': nmf_labels,
-    'Coefficient': coefs,
-    'AbsCoefficient': np.abs(coefs)
-}).sort_values(by='AbsCoefficient', ascending=False)
-
-plt.figure(figsize=(10, 7))
-sns.barplot(x='Factor', y='AbsCoefficient', data=importance_df.head(15), palette="viridis")
-plt.title("Top 15 Most Informative NMFs for Predicting Disease")
-plt.ylabel("Abs Logistic Regression Coef")
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-# --- Top NMF visualization ---
-cropped = True
-top5_nmf = importance_df['Factor'].head(6).tolist()
-top5_indices = [int(f[3:]) - 1 for f in top5_nmf]
-top5_df = pd.DataFrame(adata.obsm["X_nmf"][:, top5_indices], columns=top5_nmf)
-top5_df["sample_id"] = adata.obs["Condition"].values #Condition
-if cropped:
-    top5_df = top5_df[adata.obs['cropped'].values].copy()
-top5_long = top5_df.melt(id_vars="sample_id", var_name="Factor", value_name="Score")
-plt.figure(figsize=(23, 8))
-sns.violinplot(x="Factor", y="Score", hue="sample_id", data=top5_long, inner="box", palette="Set2")
-if cropped:
-    plt.title("Distribution of Top 6 Informative NMF Factors Across Samples (Cropped Regions)")
-else:
-    plt.title("Distribution of Top 6 Informative NMF Factors Across Samples")
-plt.legend(title="Sample ID", bbox_to_anchor=(1.05, 1), loc="upper left")
-plt.tight_layout()
-plt.show()
-
-
-import dataframe_image as dfi
-################ Identifying the top genes associated with factors
-#### uncropped - t3_7 indices
-GOF_index = [21, 1, 9, 22, 24]
-LOF_index = [0, 27, 12, 5, 19]
-
-#### uncropped - t18 indices
-GOF_index = [18, 12, 9, 20, 14]
-LOF_index = [3, 19, 23, 25]
-
-#### uncropped - t7 indices
-GOF_index = [0, 12, 16, 20, 15]
-LOF_index = [27, 4, 10]
-
-NMF_H = pd.DataFrame(adata.uns["nmf_components"]).T
-NMF_H.columns = [f'NMF{i+1}' for i in range(NMF_H.shape[1])]
-NMF_H['genes'] = adata.var_names
-print(NMF_H.shape)
-symbols= NMF_H['genes'].map(hlps.map_ensembl_to_symbol(NMF_H['genes'].tolist(), species='mouse'))
-print(symbols.head(10))
-NMF_H['symbols'] = symbols
-print(NMF_H.head(10))
-
-for index in GOF_index + LOF_index:
-    factor_name = f'NMF{index + 1}'
-    print(f"Top genes for {factor_name}:")
-    ### make a df of the gene symbol and the factor name and sort it
-    gene_df = pd.DataFrame({
-    'symbols': NMF_H['symbols'],
-    factor_name: NMF_H[factor_name]
-    })
-    gene_df.sort_values(by=factor_name, ascending=False, inplace=True)
-    gene_df.reset_index(drop=True, inplace=True)
-    # Apply styling to set the background color to white
-    styled_df = gene_df.head(35).style.set_properties(**{'background-color': 'white'})
-    styled_df = styled_df.set_properties(**{'color': 'black'})
-    styled_df
-    # Export the styled DataFrame to a PNG file
-    #outpath = f"/home/delaram/SpatialPeeler/Plots/remyelin_t3_7_NMF{index+1}_genes_df.png"
-    #outpath = f"/home/delaram/SpatialPeeler/Plots/remyelin_t18_NMF{index+1}_genes_df.png"
-    outpath = f"/home/delaram/SpatialPeeler/Plots/remyelin_t7_NMF{index+1}_genes_df.png"
-
-    dfi.export(
-        styled_df,
-        outpath,
-        table_conversion="matplotlib",  
-        dpi=300
-    )
